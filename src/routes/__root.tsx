1cebd2b2198aa25b058727efedb7cad9ee4d579d
Andres Bernal <pipejack2002@gmail.com>
2026-08-29 14:55:38 -0500

    fix(css): bump css build hash and add cache busting query param

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -21,7 +21,7 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-      { rel: "stylesheet", href: appCss },
+      { rel: "stylesheet", href: `${appCss}?v=20260829_v3` },
       { rel: "manifest", href: "/__grok/manifest.webmanifest" },
       { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
